f137a95618b4bc7e13a7f5f7e0aa8d02f4ea7620
Pat Armstrong <pat.armstrong@gmail.com>
2013-12-03 12:21:57 +1100

    rough header styling

--- app/js/app/layout.js
+++ app/js/app/layout.js
@@ -29,12 +29,41 @@ define([
     });
   };
 
+  var wordsInSpans = function() {
+    // alternate version of screenreader hell
+    var splitHeadlines = $('.headline.spans');
+    splitHeadlines.each(function() {
+      var headlineText = $(this).html().trim().split(' ');
+      for (i in headlineText) {
+        headlineText[i] = '<b class="word word-' + (headlineText.indexOf(headlineText[i]) + 1) + '">' + headlineText[i] + '</b>';   
+      }
+      var text = headlineText.join('');
+      $(this).html(text);
+      // ACTUAL HELL
+      lettersInSpans();
+    });
+  };
+
+  var lettersInSpans = function() {
+    // definitely going to screenreader hell
+    var splitHeadlineWords = $('.headline.spans .word');
+    splitHeadlineWords.each(function() {
+      var headlineText = $(this).html().trim().split('');
+      for (i in headlineText)
+          headlineText[i] = '<b class="char">' + headlineText[i] + '</b>';   
+      var text = headlineText.join('');
+      $(this).html(text);
+    });
+  };
+
   var init = function() {
     slideContainers = $('.slide-container');
     navBar = $('.navbar');
     articleFooter = $('.article-footer');
 
     sizeSlideContainers();
+    wordsInSpans();
+    // lettersInSpans();
     sizeFooter();
 
     events.trigger('layout:ready');
