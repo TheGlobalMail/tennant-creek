558fddd981dad7a2d976c5f5891e204c17bc77e2
Mark Finger <markfinger@gmail.com>
2013-12-06 16:14:35 +1100

    Progress bar now hooked up. Moved play buttons over the media assets.

--- app/js/app/media.js
+++ app/js/app/media.js
@@ -8,7 +8,6 @@ define([
 
   var slideContainers;
   var autoplayMedia;
-  var autoplayAudio;
   var videoContainers;
 
   var jsCanPlayVideo = (function() {
@@ -17,52 +16,108 @@ define([
     return !video.paused;
   })();
 
-  var playMedia = function(element) {
+  var playMedia = function(element, container) {
     element.play();
     if (element.paused !== true) {
-      videoContainers.has(element).addClass('playing');
+      container.addClass('playing');
+    }
+    if (!container.hasClass('played')) {
+      container.addClass('played')
     }
   };
 
-  var pauseMedia = function(element, preservePosition) {
+  var pauseMedia = function(element, container) {
     element.pause();
-    if (!preservePosition) {
-      element.currentTime = 0;
-    }
-    videoContainers.has(element).removeClass('playing');
+    element.currentTime = 0;
+    container.removeClass('playing');
+  };
+
+  var initProgressBar = function(element, progressBar) {
+    var durationData = $(element).data('duration');
+    durationData = durationData.split(':');
+    var minutes = parseInt(durationData[0]);
+    var seconds = parseInt(durationData[1]);
+    var duration = (minutes * 60) + seconds;
+
+    var progress = progressBar.find('.progress');
+
+    var progressUpdater = function() {
+      if (element.duration > duration) {
+        duration = element.duration;
+      }
+      var percentage = (element.currentTime / duration) * 100;
+      if (percentage > 100) {
+        percentage = 100;
+      }
+      progress.css('width', percentage + '%');
+      if (!element.paused) {
+        setTimeout(progressUpdater, 50);
+      }
+    };
+
+    progressUpdater();
   };
 
   // TODO: fade in/out
   var bindAutoplayMedia = function() {
-    _.each(autoplayMedia, function(container) {
-      var media = $(container).find('video, audio')[0];
-//      var controls = container.find('.controls');
+    _.each(autoplayMedia, function(element) {
+      var container = $(element);
+      var media = container.find('video, audio')[0];
+      var controls = container.find('.controls');
       var hasPlayed = false;
+      var progressBar = container.find('.progress-bar');
 
       if (jsCanPlayVideo) {
         scroll.track(container, {
           contained: function() {
             if (media.paused && !hasPlayed) {
-              playMedia(media);
+              playMedia(media, container);
               hasPlayed = true;
             }
           },
           exit: function() {
-            pauseMedia(media);
+            pauseMedia(media, container);
             hasPlayed = false;
           }
         });
       }
 
-//      controls.on('click', function() {
-//        if (element.paused) {
-//          playMedia(element);
-//          hasPlayed = true;
-//        } else {
-//          pauseMedia(element);
-//        }
-//      });
+      controls.on('click', function() {
+        if (media.paused) {
+          playMedia(media, container);
+          hasPlayed = true;
+        } else {
+          pauseMedia(media, container);
+        }
+      });
+
+      $(media).on('playing', function() {
+        initProgressBar(media, progressBar);
+      });
+
+      $(media).on('ended', function() {
+        container.removeClass('playing');
+      });
     });
+  };
+
+  var initMedia = function() {
+    _.each(autoplayMedia, function(element) {
+      var container = $(element);
+      var playIcon = 'icon-play';
+      if (container.find('audio').length) {
+        playIcon = 'icon-volume-up';
+      }
+      $(
+        '<div class="controls">' +
+          '<i class="play icon ' + playIcon + '"></i>' +
+          '<i class="stop icon icon-stop"></i>' +
+        '</div>' +
+        '<div class="progress-bar">' +
+          '<div class="progress" style="width: 60%;"></div>' +
+        '</div>'
+      ).appendTo(container);
+    })
   };
 
   var onEnterSlideContainer = function(element) {
@@ -84,6 +139,7 @@ define([
     autoplayMedia = $('.autoplay-when-visible');
     videoContainers = $('.video-container');
 
+    initMedia();
     setBindings();
     events.trigger('media:ready');
   };
