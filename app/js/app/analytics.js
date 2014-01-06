da7a1b9be1eca8dd75b6887643c9302b9bb45e9c
B.J. Rossiter <b.j.rossiter@gmail.com>
2014-01-06 15:51:33 +1100

    Added category to google analytics

--- app/js/app/analytics.js
+++ app/js/app/analytics.js
@@ -11,16 +11,18 @@ define([
     pointsToTrack['Read to ' + percentage + '%'] = percentage;
   });
 
+  var category = 'Progress tracking';
+  var gaCommand = '_trackEvent';
+
   var logScrollTracking = function() {
     var scrollPercentage = ((viewport.getScrollY() + window.innerHeight) / $(document).height()) * 100;
     _.each(pointsToTrack, function(percentage) {
       if (scrollPercentage >= percentage) {
-        var gaCommand = '_trackEvent';
         var eventName = 'Read to ' + percentage + '%';
         if (settings.debugAnalytics) {
-          console.log([gaCommand, eventName]);
+          console.log([gaCommand, category, eventName]);
         }
-        window._gaq && _gaq.push([gaCommand, eventName]);
+        window._gaq && _gaq.push([gaCommand, category, eventName]);
         delete pointsToTrack[eventName];
       }
     });
@@ -37,7 +39,7 @@ define([
 
   var init = function() {
     // Log the page load
-    window._gaq && _gaq.push(['_trackEvent', 'Page load']);
+    window._gaq && _gaq.push([gaCommand, category, 'Page load']);
     setBindings();
   };
 
